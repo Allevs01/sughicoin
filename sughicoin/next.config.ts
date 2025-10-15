f6111294da529f24911837a66516578fdb8039ef
Allevs01 <stefanoallevi01@gmail.com>
2025-10-15 23:52:26 +0100

    add stuff

--- sughicoin/next.config.ts
+++ sughicoin/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // << static export
-  images: { unoptimized: true }, // we’re using <img>, but keep this safe
+  /* config options here */
 };
 
 export default nextConfig;
